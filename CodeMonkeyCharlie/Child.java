8ef53af43c8d8c7b426afb15d43f89e188382076
onurmumcu <onur.mmc@gmail.com>
2018-12-30 21:42:18 -0500

    mac'ten github push etmeye calisiyorum

---
+++ CodeMonkeyCharlie/Child.java
@@ -0,0 +1,8 @@
+
+public class Child {
+public static void main(String[] args) {
+System.out.println("deneme");	
+}
+
+}
+
---
+++ Telusko/src/Add.java
@@ -0,0 +1,10 @@
+
+public class Add {
+	
+	public static void main(String args[]) {
+		
+		char c = 'A';
+		System.out.println((char)c);
+	}
+
+}
